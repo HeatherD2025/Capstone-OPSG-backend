bb7c19107419cfb16bc4090c08d61bd74a80e512
heatherd2025 <heatherdeliso@gmail.com>
2025-10-21 11:24:41 -0400

    fixed token renaming discrepancy

--- controllers/authController.js
+++ controllers/authController.js
@@ -3,9 +3,30 @@ import bcrypt from "bcrypt";
 import jwt from "jsonwebtoken";
 import "dotenv/config";
 
+const generateAccessToken = (user) => {
+  return jwt.sign(
+    {
+      id: user.id,
+      email: user.email,
+      isAdmin: user.isAdmin
+    },
+    process.env.WEB_TOKEN || process.env.ACCESS_TOKEN_SECRET,
+    { expiresIn: "15m"}
+  );
+};
+
+const generateRefreshToken = (user) => {
+  return jwt.sign(
+    { id: user.id },
+    process.env.WEB_TOKEN || process.env.ACCESS_TOKEN_SECRET,
+    { expiresIn: "1d"}
+  );
+};
+
 export const login = async (req, res, next) => {
   try {
     const { email, password } = req.body;
+
     // find user and select all info for login, password removed on res.
     const user = await prisma.user.findUnique({
       where: { email },
@@ -35,11 +56,8 @@ export const login = async (req, res, next) => {
       });
     }
 
-    const token = jwt.sign(
-      { id: user.id, email: user.email, isAdmin: user.isAdmin },
-      process.env.WEB_TOKEN,
-      { expiresIn: "24" }
-    );
+    const accessToken = generateAccessToken(user);
+    const refreshToken = generateRefreshToken(user);
 
     // Remove password from response
     const { password: _, ...userWithoutPassword } = user;
@@ -49,7 +67,8 @@ export const login = async (req, res, next) => {
       message: "Login successful",
       data: {
         user: userWithoutPassword,
-        token,
+        accessToken,
+        refreshToken,
       },
     });
   } catch (error) {
@@ -61,6 +80,13 @@ export const login = async (req, res, next) => {
 export const register = async (req, res, next) => {
   try {
     const { firstName, lastName, email, password } = req.body;
+
+    const existingUser = await prisma.user.findUnique({ where: {email} });
+    if (existingUser)
+      return res.status(400).json({
+        statusCode: 400,
+        message: "User already exists",
+  });
 
     const salt = await bcrypt.genSalt(10);
     const hashedPassword = await bcrypt.hash(password, salt);
@@ -81,15 +107,8 @@ export const register = async (req, res, next) => {
       },
     });
 
-    const token = jwt.sign(
-      {
-        id: registerUser.id,
-        email: registerUser.email,
-        isAdmin: registerUser.isAdmin,
-      },
-      process.env.WEB_TOKEN,
-      { expiresIn: "24h" }
-    );
+    const accessToken = generateAccessToken(user);
+    const refreshToken = generateRefreshToken(user);
 
     const { password: _, ...userWithoutPassword } = registerUser;
 
@@ -98,7 +117,8 @@ export const register = async (req, res, next) => {
       message: "Registration succesful",
       data: {
         user: userWithoutPassword,
-        token,
+        accessToken,
+        refreshToken,
       },
     });
   } catch (error) {
