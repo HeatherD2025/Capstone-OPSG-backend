d5a7378c6c353ebfec34d685293a5d8007fd8d79
heatherd2025 <heatherdeliso@gmail.com>
2025-11-04 09:56:08 -0500

    forgot to recomment in login/register

--- controllers/authController.js
+++ controllers/authController.js
@@ -23,109 +23,109 @@ import "dotenv/config";
 //   );
 // };
 
-// export const login = async (req, res, next) => {
-//   try {
-//     const { email, password } = req.body;
+export const login = async (req, res, next) => {
+  try {
+    const { email, password } = req.body;
 
-//     // find user and select all info for login, password removed on res.
-//     const user = await prisma.user.findUnique({
-//       where: { email },
-//       select: {
-//         id: true,
-//         firstName: true,
-//         lastName: true,
-//         email: true,
-//         isAdmin: true,
-//         password: true,
-//       },
-//     });
+    // find user and select all info for login, password removed on res.
+    const user = await prisma.user.findUnique({
+      where: { email },
+      select: {
+        id: true,
+        firstName: true,
+        lastName: true,
+        email: true,
+        isAdmin: true,
+        password: true,
+      },
+    });
 
-//     if (!user) {
-//       return res.status(404).json({
-//         statusCode: 404,
-//         message: "User not found",
-//       });
-//     }
+    if (!user) {
+      return res.status(404).json({
+        statusCode: 404,
+        message: "User not found",
+      });
+    }
 
-//     const isPasswordValid = await bcrypt.compare(password, user.password);
+    const isPasswordValid = await bcrypt.compare(password, user.password);
 
-//     if (!isPasswordValid) {
-//       return res.status(401).json({
-//         statusCode: 401,
-//         message: "Invalid password",
-//       });
-//     }
+    if (!isPasswordValid) {
+      return res.status(401).json({
+        statusCode: 401,
+        message: "Invalid password",
+      });
+    }
 
-//     const accessToken = generateAccessToken(user);
-//     const refreshToken = generateRefreshToken(user);
+    const accessToken = generateAccessToken(user);
+    const refreshToken = generateRefreshToken(user);
 
-//     // Remove password from response
-//     const { password: _, ...userWithoutPassword } = user;
+    // Remove password from response
+    const { password: _, ...userWithoutPassword } = user;
 
-//     res.status(200).json({
-//       statusCode: 200,
-//       message: "Login successful",
-//       data: {
-//         user: userWithoutPassword,
-//         token: accessToken,
-//         refreshToken,    
-//       },
-//     });
-//   } catch (error) {
-//     console.error("Login error", error);
-//     next(error);
-//   }
-// };
+    res.status(200).json({
+      statusCode: 200,
+      message: "Login successful",
+      data: {
+        user: userWithoutPassword,
+        token: accessToken,
+        refreshToken,    
+      },
+    });
+  } catch (error) {
+    console.error("Login error", error);
+    next(error);
+  }
+};
 
-// export const register = async (req, res, next) => {
-//   try {
-//     const { firstName, lastName, email, password } = req.body;
+export const register = async (req, res, next) => {
+  try {
+    const { firstName, lastName, email, password } = req.body;
 
-//     const existingUser = await prisma.user.findUnique({ where: {email} });
-//     if (existingUser)
-//       return res.status(400).json({
-//         statusCode: 400,
-//         message: "User already exists",
-//   });
+    const existingUser = await prisma.user.findUnique({ where: {email} });
+    if (existingUser)
+      return res.status(400).json({
+        statusCode: 400,
+        message: "User already exists",
+  });
 
-//     const salt = await bcrypt.genSalt(10);
-//     const hashedPassword = await bcrypt.hash(password, salt);
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(password, salt);
 
-//     const registerUser = await prisma.user.create({
-//       data: {
-//         firstName,
-//         lastName,
-//         email,
-//         password: hashedPassword,
-//       },
-//       select: {
-//         id: true,
-//         firstName: true,
-//         lastName: true,
-//         email: true,
-//         isAdmin: true,
-//       },
-//     });
+    const registerUser = await prisma.user.create({
+      data: {
+        firstName,
+        lastName,
+        email,
+        password: hashedPassword,
+      },
+      select: {
+        id: true,
+        firstName: true,
+        lastName: true,
+        email: true,
+        isAdmin: true,
+      },
+    });
 
-//     const accessToken = generateAccessToken(registerUser);
-//     const refreshToken = generateRefreshToken(registerUser);
+    const accessToken = generateAccessToken(registerUser);
+    const refreshToken = generateRefreshToken(registerUser);
 
-//     const { password: _, ...userWithoutPassword } = registerUser;
+    const { password: _, ...userWithoutPassword } = registerUser;
 
-//     return res.status(201).json({
-//       statusCode: 201,
-//       message: "Registration succesful",
-//       data: {
-//         user: userWithoutPassword,
-//         token: accessToken,
-//         refreshToken, 
-//       },
-//     });
-//   } catch (error) {
-//     console.error("Registration error", error);
-//     next(error);
-//   }
-// };
+    return res.status(201).json({
+      statusCode: 201,
+      message: "Registration succesful",
+      data: {
+        user: userWithoutPassword,
+        token: accessToken,
+        refreshToken, 
+      },
+    });
+  } catch (error) {
+    console.error("Registration error", error);
+    next(error);
+  }
+};
 
 // TEST CODE
 const generateAccessToken = (user) => {
