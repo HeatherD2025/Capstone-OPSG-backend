ac441904b467c71c7d08905d98f060dd18da89c2
heatherd2025 <heatherdeliso@gmail.com>
2026-02-06 11:34:20 -0500

    fixed user access to dashboard

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -7,6 +7,9 @@ import {
 
 const router = express.Router();
 
+router.get("/dashboard", isLoggedIn, (req, res) => {
+  res.status(200).json({ message: "Welcome!" });
+});
 router.get("/me", isLoggedIn, getCurrentUser);
 router.put("/me", isLoggedIn, updateUserProfile);
 // router.put("/updateUserProfile/:userId", isLoggedIn, updateUserProfile);  // future use - enable pass reset and user delete only
