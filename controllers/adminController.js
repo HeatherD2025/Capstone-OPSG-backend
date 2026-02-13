f3ed8b7b6d7b56793769003fc7c5a4cf58b73ba3
heatherd2025 <heatherdeliso@gmail.com>
2026-02-13 14:53:22 -0500

    fixed syntax in prisma.user.findUnique

--- controllers/adminController.js
+++ controllers/adminController.js
@@ -1,35 +1,80 @@
 import { Prisma } from '@prisma/client';
 import prisma from '../common/client.js';
 
-export const getAllUsers = async (req, res, next) => {
-  try {
-    const allUsers = await prisma.user.findMany();
+export const getUsers = async (req, res, next) => {
+  const { term, role, active, createdAfter, page=1, limit=20} = req.query
 
-    // fail and success responses
-    if (allUsers.length === 0) {
-      return res.status(404).json({
+  let where = {};
+
+  if (term) {
+    const words = term.trim().split(/\s+/);
+    where.OR = [
+      {email: {contains: term, mode: 'insensitive'}},
+      {firstName: {contains: term, mode: 'insensitive'}},
+      {lastName: {contains: term, mode: 'insensitive'}},
+      ...words.map(...)
+    ]
+    
+  }
+  try {
+    const allUsers = await prisma.user.findMany({
+    where: {},
+      select: {
+        id: true,
+        firstName: true,
+        lastName: true,
+        email: true,
+        commpany: true,
+        isAdmin: true,
+      },
+    })
+    if (!firstName || !lastName || !email) {
+      return res.status(404).json ({
         statusCode: 404,
-        message: "No users found.",
-        data: []
-      });
+        message: "User not found"
+      })
+
     }
 
-    res.status(200).json({
-        statusCode: 200,
-        message: "Users sucessfully retrieved",
-        data: allUsers
-    })
+    // if term  - add OR conditions for firstName/lastName/email
+    // if role filter
+    // if active filter
+    // if createdAfter date filter
+
   } catch (error) {
     next(error);
   }
 };
 
+
+// export const getAllUsers = async (req, res, next) => {
+//   try {
+//     const allUsers = await prisma.user.findMany();
+
+//     // fail and success responses
+//     if (allUsers.length === 0) {
+//       return res.status(200).json({
+//         statusCode: 200,
+//         message: "OK",
+//         data: []
+//       });
+//     }
+
+//     res.status(200).json({
+//         statusCode: 200,
+//         message: "Users sucessfully retrieved",
+//         data: allUsers
+//     })
+//   } catch (error) {
+//     next(error);
+//   }
+// };
+
 export const getUserById = async (req, res, next) => {
   try {
     const { userId } = req.params;
 
-    
-    const getUser = await prisma.findUnique({
+    const getUser = await prisma.user.findUnique({
       where: {id: userId},
       select: {
         id: true,
@@ -40,7 +85,7 @@ export const getUserById = async (req, res, next) => {
       },
     });
 
-    if (!user) {
+    if (!getUser) {
       return res.status(404).json({
         statusCode: 404,
         message: "User not found",
@@ -57,59 +102,59 @@ export const getUserById = async (req, res, next) => {
   }
 };
 
-export const searchUsers = async (req, res, next) => {
-  try {
-    const { term } = req.query;
-    if (!term) {
-      return res.status(400).json({
-        statusCode: 400,
-        message: "Please provide a search term"
-      });
-    }
+// export const searchUsers = async (req, res, next) => {
+//   try {
+//     const { term } = req.query;
+//     if (!term) {
+//       return res.status(400).json({
+//         statusCode: 400,
+//         message: "Please provide a search term"
+//       });
+//     }
 
-    const words = term.trim().split(/\s+/)
+//     const words = term.trim().split(/\s+/)
 
-    // find user and select all info for login, password removed on res.
-    const users = await prisma.user.findMany({
-      where: {
-        OR: [
-          { email: { contains: term, mode: "insensitive" } },
-          ...words.map((word) => ({
-            AND: [
-              { firstName: { contains: word, mode: "insensitive" } },
-              { lastName: { contains: word, mode: "insensitive" } },
-            ],
-          })),
-          { firstName: { contains: term, mode: "insensitive" } },
-          { lastName: { contains: term, mode: "insensitive" } },
-        ],
-      },
-      select: {
-        id: true,
-        firstName: true,
-        lastName: true,
-        email: true,
-        isAdmin: true,
-      },
-    });
+//     // find user and select all info for login, password removed on res.
+//     const users = await prisma.user.findMany({
+//       where: {
+//         OR: [
+//           { email: { contains: term, mode: "insensitive" } },
+//           ...words.map((word) => ({
+//             AND: [
+//               { firstName: { contains: word, mode: "insensitive" } },
+//               { lastName: { contains: word, mode: "insensitive" } },
+//             ],
+//           })),
+//           { firstName: { contains: term, mode: "insensitive" } },
+//           { lastName: { contains: term, mode: "insensitive" } },
+//         ],
+//       },
+//       select: {
+//         id: true,
+//         firstName: true,
+//         lastName: true,
+//         email: true,
+//         isAdmin: true,
+//       },
+//     });
 
-    if (!users || users.length === 0) {
-      return res.status(404).json({
-        statusCode: 404,
-        message: "No users found",
-      });
-    }
+//     if (!users || users.length === 0) {
+//       return res.status(200).json({
+//         statusCode: 200,
+//         message: "OK",
+//       });
+//     }
 
-    res.status(200).json({
-      statusCode: 200,
-      message: "User found",
-      data: users,
-    });
-  } catch (error) {
-    console.error("Search user error", error);
-    next(error);
-  }
-};
+//     res.status(200).json({
+//       statusCode: 200,
+//       message: "User found",
+//       data: users,
+//     });
+//   } catch (error) {
+//     console.error("Search user error", error);
+//     next(error);
+//   }
+// };
 
 
 export const deleteUserById = async (req, res, next) => {
