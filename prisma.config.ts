7faa9aa8976a91a69843c75943407581153f43c7
heatherd2025 <heatherdeliso@gmail.com>
2026-02-17 18:04:42 -0500

    reverted back to v4

--- prisma.config.ts
+++ prisma.config.ts
@@ -1,5 +1,14 @@
-export default {
+import "dotenv/config";
+import { defineConfig, env } from "prisma/config";
+
+export default defineConfig({
+  schema: "prisma/schema.prisma",
   migrations: {
+    path: "prisma/migrations",
     seed: "node ./prisma/seed.js",
   },
-};
+  datasource: {
+    url: env("DATABASE_URL"),
+  },
+});
+
