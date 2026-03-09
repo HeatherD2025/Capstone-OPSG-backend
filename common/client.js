1df65e4aa54d4410ef3e1b74d9a31d1cc896607b
heatherd2025 <heatherdeliso@gmail.com>
2026-03-09 14:35:44 -0400

    client js test

--- common/client.js
+++ common/client.js
@@ -1,10 +1,12 @@
-import { PrismaClient } from '@prisma/client'
-import { withAccelerate } from '@prisma/extension-accelerate'
+import { PrismaClient } from '@prisma/client/edge'; // Note the /edge
+import { withAccelerate } from '@prisma/extension-accelerate';
 
 const prisma = new PrismaClient({
   datasources: {
-    db: { url: process.env.ACCELERATE_URL },
+    db: {
+      url: process.env.ACCELERATE_URL,
+    },
   },
 }).$extends(withAccelerate());
 
-export default prisma
+export default prisma;
