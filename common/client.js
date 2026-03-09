bfc5dd536796a46106287829ce8a6598c8a6c194
heatherd2025 <heatherdeliso@gmail.com>
2026-03-09 13:10:11 -0400

    accelerate updates to client.js and package.json

--- common/client.js
+++ common/client.js
@@ -1,8 +1,12 @@
-import { PrismaClient } from '@prisma/client';
-import { withAccelerate } from '@prisma/extension-accelerate';
+import { PrismaClient } from '@prisma/client'
+import { withAccelerate } from '@prisma/extension-accelerate'
 
-const prisma = new PrismaClient().$extends(withAccelerate());
+const prisma = new PrismaClient({
+  datasources: {
+    db: {
+      url: process.env.ACCELERATE_URL,
+    },
+  },
+}).$extends(withAccelerate());
 
 export default prisma;
-
-
