917ae51359140b31849a298d49aaf3b326224757
heatherd2025 <heatherdeliso@gmail.com>
2026-03-09 14:29:20 -0400

    typo fix

--- common/client.js
+++ common/client.js
@@ -1,5 +1,10 @@
+import { PrismaClient } from '@prisma/client'
+import { withAccelerate } from '@prisma/extension-accelerate'
+
 const prisma = new PrismaClient({
   datasources: {
     db: { url: process.env.ACCELERATE_URL },
   },
 }).$extends(withAccelerate());
+
+export default prisma
